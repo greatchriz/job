22596b3a09bb9e9a890fb3e8d37b00803ac3397d
greatchriz <89561544+greatchriz@users.noreply.github.com>
2024-02-26 09:29:52 +0100

    IJIK

--- .history/app/Models/Job_20240226092936.php
+++ .history/app/Models/Job_20240226092936.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Company;
 use App\Models\JobApplication;
+use App\Models\Message;
+use App\Models\Location;
+
 
 class Job extends Model
 {
@@ -23,4 +26,11 @@ class Job extends Model
     {
         return $this->hasMany(JobApplication::class);
     }
+
+    // location
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Company;
 use App\Models\JobApplication;
 use App\Models\Message;
-use App\Models\location;
+use App\Models\Location;
 
 
 class Job extends Model
